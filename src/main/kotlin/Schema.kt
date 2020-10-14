b7f9e1b9a87766c285e1143d99df5f1604ee3ae9
Rick Hawes <richard.hawes@cms.hhs.gov>
2020-10-14 16:50:30 -0700

    Formating improvements

--- src/main/kotlin/Schema.kt
+++ src/main/kotlin/Schema.kt
@@ -7,21 +7,23 @@ data class Schema(
     //val order: List<String>?, coming soon reorder the elements
     val extends: String? = null,
     val parent: Schema? = null, // fixup after loading into manager
- ) {
+) {
     data class Hl7Segment(val name: String, val template: String)
 
     val allElements: List<Element>
-        get() { return if (parent == null) elements else parent.allElements + elements }
+        get() {
+            return if (parent == null) elements else parent.allElements + elements
+        }
 
-    fun findElement(name: String) : Element? {
-        return elements.find { element -> element.name == name } ?: parent?.findElement(name)
+    fun findElement(name: String): Element? {
+        return elements.find { it.name == name } ?: parent?.findElement(name)
     }
 
-    fun unionElements(otherSchema: Schema) : List<Element> {
+    fun unionElements(otherSchema: Schema): List<Element> {
         return elements + otherSchema.diffElements(this)
     }
 
-    fun diffElements(otherSchema: Schema) : List<Element> {
+    fun diffElements(otherSchema: Schema): List<Element> {
         // Kind of brute force, but simple
         return elements.filter { otherSchema.findElement(it.name) == null }
     }
